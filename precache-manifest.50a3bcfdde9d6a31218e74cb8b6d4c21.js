dadd83043cb08255a415a73bea3851458be6b00b
kingslayer2274 <farsalzalam22@gmail.com>
2019-09-19 13:52:21 +0200

    Updates

---
+++ precache-manifest.50a3bcfdde9d6a31218e74cb8b6d4c21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639e56c1126ef6da6c7dea16ca2a5b72",
+    "url": "/RoboFriends_Redux/index.html"
+  },
+  {
+    "revision": "b3cac5492b7518a99887",
+    "url": "/RoboFriends_Redux/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "807d94c270f68ab67f6b",
+    "url": "/RoboFriends_Redux/static/css/main.8b95b15d.chunk.css"
+  },
+  {
+    "revision": "b3cac5492b7518a99887",
+    "url": "/RoboFriends_Redux/static/js/2.c76b53a4.chunk.js"
+  },
+  {
+    "revision": "807d94c270f68ab67f6b",
+    "url": "/RoboFriends_Redux/static/js/main.a8b58b32.chunk.js"
+  },
+  {
+    "revision": "e5be5d1602ba0e11f346",
+    "url": "/RoboFriends_Redux/static/js/runtime~main.9944340f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends_Redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
